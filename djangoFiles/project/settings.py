7fd46f02e3201eb3009b2451b8554a8de7d155ed
Italo Perez <iperezmba@gmail.com>
2020-02-10 03:49:30 +0000

    Connectors has been added.

--- djangoFiles/project/settings.py
+++ djangoFiles/project/settings.py
@@ -77,10 +77,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER':'myuser',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432',
+
 }
+
+	}
+
+
+
 
 
 # Password validation
@@ -121,7 +130,7 @@ USE_TZ = True
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio/static/')
+    os.path.join(BASE_DIR, 'project/static/')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
